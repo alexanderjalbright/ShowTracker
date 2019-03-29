7ed2e58d2dd65708938dbb6eb4498d0a030a623c
Mary McGeary <malicemcgeary@gmail.com>
2019-03-29 17:33:35 -0400

    Get returns all shows test passed, show controller created

---
+++ backend/ShowTracker.Tests/ShowControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using ShowTracker.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ShowTracker.Tests
+{
+    public class ShowControllerTests
+    {
+        [Fact]
+        public void Get_Returns_All_Shows()
+        {
+            var undertest = new ShowController();
+
+            var result = undertest.Get();
+
+            Assert.Equal(2, result.Value.Count());
+        }
+    }
+}
---
+++ backend/ShowTracker/Controllers/ShowController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShowTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Show>> Get()
+        {
+            var showList = new List<Show>()
+            { new Show(), new Show() };
+
+            return showList;
+        }
+    }
+}
