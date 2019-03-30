ed74ceb69036b42dcbd40a6bc7aea3a46500b9e3
Mary McGeary <malicemcgeary@gmail.com>
2019-03-30 14:09:05 -0400

    added seasons to seed data

---
+++ backend/ShowTracker/Migrations/20190330180804_SeasonsAdded.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShowTracker.Migrations
+{
+    public partial class SeasonsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 1,
+                column: "Seasons",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 2,
+                column: "Seasons",
+                value: 30);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 3,
+                column: "Seasons",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 4,
+                column: "Seasons",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 5,
+                column: "Seasons",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 6,
+                column: "Seasons",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 7,
+                column: "Seasons",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 8,
+                column: "Seasons",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 9,
+                column: "Seasons",
+                value: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 1,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 2,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 3,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 4,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 5,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 6,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 7,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 8,
+                column: "Seasons",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "ShowId",
+                keyValue: 9,
+                column: "Seasons",
+                value: 0);
+        }
+    }
+}
