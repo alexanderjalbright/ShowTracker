e2f8176f09112ee811d21cdb1bc5e85dc85fc3a3
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-03-30 16:22:35 -0400

    Added delete button

--- frontend/src/Show.js
+++ frontend/src/Show.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 
 export default class Show extends Component {
+  deleteShow = id => {
+    const url = "https://localhost:44387/api/show/" + id;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          console.log("good");
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
   render() {
     const { show } = this.props;
     return (
@@ -8,6 +25,7 @@ export default class Show extends Component {
         <h2>{show.name}</h2>
         <small>({show.seasons} Seasons)</small>
         <p>{show.description}</p>
+        <button onClick={() => this.deleteShow(show.showId)}>Delete</button>
       </li>
     );
   }
