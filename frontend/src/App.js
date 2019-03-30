7a9a313a5f179204045563ffbac0f94067619915
Alex Albright <alexanderjalbright@gmail.com>
2019-03-30 13:51:25 -0400

    Genre and Show models are displayed and saved in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,40 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      genres: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://localhost:44387/api/genre")
+      .then(res => res.json())
+      .then(json => this.setState({ genres: json }));
+  }
+
+  showList = shows =>
+    shows.map(show => (
+      <li key={show.showId}>
+        <h2>Show: {show.name}</h2>
+        <p>Description: {show.description}</p>
+        <p>Seasons: {show.seasons}</p>
+      </li>
+    ));
+
   render() {
+    const genreList = this.state.genres.map(genre => (
+      <li key={genre.genreId} className="genres">
+        <h1>Genre: {genre.name}</h1>
+        <p>Description: {genre.description}</p>
+        <ul>{this.showList(genre.shows)}</ul>
+      </li>
+    ));
+
     return (
       <div className="App">
-        <p>placeholder</p>
+        <ul>{genreList}</ul>
       </div>
     );
   }
