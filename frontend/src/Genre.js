ce022638810071acfeb6865db64edd74a0ab095c
Alex Albright <alexanderjalbright@gmail.com>
2019-03-30 18:50:03 -0400

    Fixed bug where add show button only displays in 1st genre

--- frontend/src/Genre.js
+++ frontend/src/Genre.js
@@ -3,15 +3,15 @@ import Show from "./Show";
 
 export default class Genre extends Component {
   displayAddForm = () => {
-    const add = document.querySelector(".add");
+    const add = document.querySelector(`.add${this.props.genre.genreId}`);
     add.style.display = "none";
-    const form = document.querySelector(".form");
+    const form = document.querySelector(`.form${this.props.genre.genreId}`);
     form.style.display = "block";
   };
   cancel = () => {
-    const add = document.querySelector(".add");
+    const add = document.querySelector(`.add${this.props.genre.genreId}`);
     add.style.display = "block";
-    const form = document.querySelector(".form");
+    const form = document.querySelector(`.form${this.props.genre.genreId}`);
     form.style.display = "none";
     this.props.setName("");
     this.props.setDescription("");
@@ -40,18 +40,36 @@ export default class Genre extends Component {
         <h1>{genre.name}</h1>
         <p>{genre.description}</p>
         <ul>{showList}</ul>
-        <div className="form">
-          <input type="text" value={name} onChange={this.nameChange} />
-          <input
-            type="text"
-            value={description}
-            onChange={this.descriptionChange}
-          />
-          <input type="number" value={seasons} onChange={this.seasonsChange} />
-          <button onClick={this.submitClick}>Submit</button>
-          <button onClick={this.cancel}>Cancel</button>
+        <div className={`form form${genre.genreId}`}>
+          <div>
+            <label>Name:</label>
+            <input type="text" value={name} onChange={this.nameChange} />
+          </div>
+          <div>
+            <label>Description:</label>
+            <input
+              type="text"
+              value={description}
+              onChange={this.descriptionChange}
+            />
+          </div>
+          <div>
+            <label>Seasons:</label>
+            <input
+              type="number"
+              value={seasons}
+              onChange={this.seasonsChange}
+            />
+          </div>{" "}
+          <div>
+            <button onClick={this.submitClick}>Submit</button>
+            <button onClick={this.cancel}>Cancel</button>
+          </div>
         </div>
-        <button className="add" onClick={this.displayAddForm}>
+        <button
+          className={`add add${genre.genreId}`}
+          onClick={this.displayAddForm}
+        >
           Add Show
         </button>
       </li>
